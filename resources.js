2b4b271a5488dc77262651fea2d10f9efc10d0e4
Jared <j.graham.3030@gmail.com>
2022-08-19 15:39:59 -0700

    Add web dev resources to resources.js and export the object to server.js

---
+++ resources.js
@@ -0,0 +1,117 @@
+module.exports = {
+    html: {
+        sites: {
+            codecademy: 'https://www.codecademy.com/learn/learn-html',
+            freecodecamp: 'https://www.freecodecamp.org/learn/2022/responsive-web-design/',
+            mdn: 'https://developer.mozilla.org/en-US/docs/Learn/HTML'
+        },
+        videos: {
+            'programming with mosh': 'https://www.youtube.com/watch?v=qz0aGYrrlhU',
+            'traversy media': 'https://www.youtube.com/watch?v=UB1O30fR-EE',
+            freecodecamp: 'https://www.youtube.com/watch?v=pQN-pnXPaVg'
+        }
+    },
+    css: {
+        sites: {
+            mdn: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+            codecademy: 'https://www.codecademy.com/learn/learn-css',
+            freecodecamp: 'https://www.freecodecamp.org/learn/2022/responsive-web-design/'
+        },
+        videos: {
+            'traversy media': 'https://www.youtube.com/watch?v=yfoY53QXEnI',
+            'web dev simplified': 'https://www.youtube.com/watch?v=1PnVor36_40'
+        }
+    },
+    specificity: {
+        sites: {
+            mdn: 'https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity',
+            'css tricks': 'https://css-tricks.com/specifics-on-css-specificity/'
+        },
+        videos: {
+            'web dev simplified': 'https://www.youtube.com/watch?v=CHyPGSpIhSs',
+            'kevin powell': 'https://www.youtube.com/watch?v=c0kfcP_nD9E'
+        }
+    },
+    'box model': {
+        sites: {
+            mdn: 'https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Box_Model/Introduction_to_the_CSS_box_model',
+            'css tricks': 'https://css-tricks.com/the-css-box-model/'
+        },
+        videos: {
+            'kevin powell': 'https://www.youtube.com/watch?v=M6coJNLFBWI',
+            'colt steele': 'https://www.youtube.com/watch?v=M1xEi_BBW1I'
+        }
+    },
+    positioning: {
+        sites: {
+            mdn: 'https://developer.mozilla.org/en-US/docs/Web/CSS/position',
+            'css tricks': 'https://css-tricks.com/almanac/properties/p/position/'
+        },
+        videos: {
+            'kevin powell': 'https://www.youtube.com/watch?v=P6UgYq3J3Qs',
+            'web dev simplified': 'https://www.youtube.com/watch?v=jx5jmI0UlXU'
+        }
+    },
+    display: {
+        sites: {
+            mdn: 'https://developer.mozilla.org/en-US/docs/Web/CSS/display',
+            'css tricks': 'https://css-tricks.com/almanac/properties/d/display/'
+        },
+        videos: {
+            'kevin powell': 'hhttps://www.youtube.com/watch?v=x_i2gga-sYg',
+            'web dev simplified': 'https://www.youtube.com/watch?v=Qf-wVa9y9V4'
+        }
+    },
+    layout: {
+        sites: {
+            mdn: 'https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout',
+            'learn css layout': 'https://learnlayout.com/'
+        },
+        videos: {
+            'google chrome developers': 'https://www.youtube.com/watch?v=yMEjLBKyvEg',
+            'kevin powell': 'https://www.youtube.com/watch?v=R7gqJkdc5dM'
+        }
+    },
+    javascript: {
+        sites: {
+            mdn: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+            'javascript.info': 'https://javascript.info/',
+            'eloquent javascript': 'https://eloquentjavascript.net/'
+        },
+        videos: {
+            'freecodecamp': 'https://www.youtube.com/watch?v=jS4aFq5-91M',
+            'traversy media': 'https://www.youtube.com/watch?v=hdI2bqOjy3c',
+            'programming with mosh': 'https://www.youtube.com/watch?v=W6NZfCO5SIk'
+        }
+    },
+    dom: {
+        sites: {
+            mdn: 'https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Introduction',
+            'eloquent javascript': 'https://eloquentjavascript.net/14_dom.html'
+        },
+        videos: {
+            'the net ninja': 'https://www.youtube.com/watch?v=wKBu_dEaF9E',
+            'web dev simplified': 'https://www.youtube.com/watch?v=y17RuWkWdn8'
+        }
+    },
+    this: {
+        sites: {
+            mdn: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this',
+            'javascript tutorial': 'https://www.javascripttutorial.net/javascript-this/'
+        },
+        videos: {
+            'programming with mosh': 'https://www.youtube.com/watch?v=gvicrj31JOM',
+            'fireship': 'https://www.youtube.com/watch?v=YOlr79NaAtQ'
+        }
+    },
+    inheritance: {
+        sites: {
+            mdn: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain',
+            'javascript.info': 'https://javascript.info/prototype-inheritance'
+        },
+        videos: {
+            'fun fun function': 'https://www.youtube.com/watch?v=riDVvXZ_Kb4',
+            'web dev simplified': 'https://www.youtube.com/watch?v=jx5jmI0UlXU'
+        }
+    }
+}
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+const resources = require('./resources.js')
 const PORT = 8003
 
 app.get('/', (req, res) => {
-    res.send('<h1>hello world</h1>')
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(PORT, () => {
