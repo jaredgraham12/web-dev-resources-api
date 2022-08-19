e65af88a49051bcff2d7245f4615e1e65799d410
Jared <j.graham.3030@gmail.com>
2022-08-19 15:50:22 -0700

    Add routes for a specific web dev topic and all web dev topics

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/api/resources', (req, res) => {
+    res.json(resources)
+})
+
+app.get('/api/resources/:topic', (req, res) => {
+    const topic = req.params.topic
+    if (resources[topic]) {
+        res.json(resources[topic])
+    } else {
+        res.json({error: 'topic not found'})
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
